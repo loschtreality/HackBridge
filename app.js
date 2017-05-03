2dc6a654bd3185bf62eb1954f16d2998eff80362
Hieu <Hieu@50-204-120-225-static.hfc.comcastbusiness.net>
2017-05-03 03:57:28 -0700

    deleted commented out code

--- app.js
+++ app.js
@@ -32,21 +32,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/api', api);
 app.use('/volunteer', volunteer);
-app.use('/student',student)
+app.use('/student', student)
 app.use('/admin', admin)
 app.use('/users/:id', userShow)
 
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
