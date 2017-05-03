cfff41b733fb80ea3781c5508415ca846d1ed97e
Hieu <Hieu@50-204-120-225-static.hfc.comcastbusiness.net>
2017-05-02 21:46:13 -0700

    added tables to admin.hbs

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET admin page. */
-router.get('/admin', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('admin', { title: 'admin site' });
 });
 
