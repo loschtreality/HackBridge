c3eee00883a5ab48720568e417ae3002f50ef9f1
Michael Loschiavo <losch103@gmail.com>
2017-05-02 20:28:27 -0700

    add syntax hotfix

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var hbs = require('hbs');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var api = require('./routes/api');
 var regirstation = require('./routes/registration');
 
 var app = express();
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/api', users);
+app.use('/api', api);
 app.use('/registration',regirstation)
 
 // catch 404 and forward to error handler
