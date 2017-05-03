618f61a428309c417e37dffda45d09f9d026cb0f
Hieu <Hieu@50-204-120-225-static.hfc.comcastbusiness.net>
2017-05-02 20:31:20 -0700

    included admin route to app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var hbs = require('hbs');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var regirstation = require('./routes/registration');
+var admin = require('./routes/admin');
 
 var app = express();
 
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/api', users);
-app.use('/registration',regirstation)
+app.use('/admin', admin)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
