a3e195e565945b6bcdd24a8a9168f7e59d543591
Michael Loschiavo <losch103@gmail.com>
2017-05-02 20:03:01 -0700

    add ajax. add volunteer form secton.

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var hbs = require('hbs');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+//handlebars partials setup
+hbs.registerPartials(__dirname + '/views/shared')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,2 +1,37 @@
-console.info("Hello!")
-$("h1").css("color", "red")
+/**
+ * Creates accordian dropdown for volunteer signup
+ */
+$("#volunteer-btn").click(function (e) {
+  if ($(this).hasClass("btn-danger")) {
+    $(this).removeClass("btn-danger").addClass("btn-warning")
+    $(this).text("Sign Up")
+  } else {
+    $(this).removeClass("btn-warning").addClass("btn-danger")
+    $(this).text("Close")
+  }
+})
+
+/**
+ * Send request to server from volunteer form submission
+ */
+$("#volunteer-form").on("submit", function (e) {
+  e.preventDefault()
+  const formData = $(this).serializeArray()
+                  .reduce((respObj, current) => {
+                    respObj[current.name] = current.value
+                    return respObj
+                  }, {})
+
+  $.ajax({
+    url: "localhost:3000/api/volunteers",
+    method: "POST",
+    body: formData,
+    dataType: "json",
+    success: function(resp) {
+      console.info("This successfully posted")
+    },
+    error: function(err) {
+      console.warn(err)
+    }
+  })
+})
